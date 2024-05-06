78cde4553e067112afc324a2eb3835f7b23ae150
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-05-06 13:49:21 +0530

    push for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <Routes>
         <Route path={'/'} element={<Style_01_Display/>}/>
+          <Route path={'/cv'} element={<Style_01_Display/>}/>
       </Routes>
     </>
   )
